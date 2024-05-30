af8c9313bf142179aa2adef43922869d1fb81f5d
sachinsoumya <pandasoumyasachin@gmail.com>
2024-05-30 16:33:04 +0530

    updated

--- src/Components/GptSearch.js
+++ src/Components/GptSearch.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import GptMovieSuggestions from './GptMovieSuggestions'
-import GptSearchBar from './GptSearchBar'
-import { BACKGROUND_IMAGE } from '../Utils/constant'
+import React from "react";
+import GptMovieSuggestions from "./GptMovieSuggestions";
+import GptSearchBar from "./GptSearchBar";
+import { BACKGROUND_IMAGE } from "../Utils/constant";
 
 const GptSearch = () => {
-  console.log(process.env.REACT_APP_OPEN_AI_KEY )
   return (
     <div>
-        <div className="absolute w-full h-full -z-30">
+      <div className="absolute w-full h-full -z-30">
         <img
           src={BACKGROUND_IMAGE}
           alt="Background_image"
           className="w-full h-full object-cover fixed"
         />
       </div>
-        <GptSearchBar />
-        <GptMovieSuggestions />
+      <GptSearchBar />
+      <GptMovieSuggestions />
     </div>
-  )
-}
+  );
+};
 
-export default GptSearch
+export default GptSearch;
